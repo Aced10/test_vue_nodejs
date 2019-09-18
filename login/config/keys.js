c63fad102481bbbb7809662eed09cea98635fb42
Alfonso Calero Espitia <acedi10@hotmail.com>
2019-09-18 12:01:57 -0500

    Delete keys.js

--- login/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	google: {
-		clientID:'284834312914-7h7ph273f8ab3fd6lldooj051c3omc0n.apps.googleusercontent.com',
-		clientSecret:'8HxkC8pyBrjJhQmWR2wFKC8I'
-	},
-	secret: 'TFCA97BRHO'
-}
